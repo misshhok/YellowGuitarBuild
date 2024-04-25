ca8b9de4c7ce1dcc08027f06239ddb51cc866109
SmirnovMA <Mikhail.Smirnov@gorparkovki.ru>
2024-04-25 10:56:47 +0300

    feat: убрать маппер

--- src/main/java/ru/bsu/yellowguitarbend/infrastructure/config/SpringComponentMapper.java
+++
@@ -1,8 +0,0 @@
-package ru.bsu.yellowguitarbend.infrastructure.config;
-
-import org.mapstruct.MapperConfig;
-import org.mapstruct.MappingConstants.ComponentModel;
-
-@MapperConfig(componentModel = ComponentModel.SPRING)
-public interface SpringComponentMapper {
-}
